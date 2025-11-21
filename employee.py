c75ca102b5bdf46394a1e3b6ece1a377e1d45e1b
Akhtarnaaz <01fe24ica054@kletech.ac.in>
2025-11-21 11:52:59 +0530

    Function to Display Employee details and pytest

--- employee.py
+++ employee.py
@@ -1,6 +1,6 @@
 # employee.py
 
-def get_employee_info(name, emp_id, department, salary):
+def employee_info(name, emp_id, department, salary):
     """
     Returns a formatted string containing employee details.
     """
@@ -8,9 +8,15 @@ def get_employee_info(name, emp_id, department, salary):
         f"Employee Name: {name}\n"
         f"Employee ID: {emp_id}\n"
         f"Department: {department}\n"
-        f"Salary: {salary:.2f}"
+        f"Salary: {salary}"
     )
 
-# Example usage (optional)
+
+
 if __name__ == "__main__":
-    print(get_employee_info("Gabriel", "EPM101", "IT", 78000))
+    name = "Bob"
+    emp_id = "E123"
+    department = "IT"
+    salary = 65000
+
+    print(employee_info(name, emp_id, department, salary))
